4e289e7c47f56f657ab247d3b043ecd0c64ec980
osa1 <omeragacan@gmail.com>
2012-06-30 04:48:25 +0300

    draw tiles continuously while left mouse is pressed

--- editor.lua
+++ editor.lua
@@ -13,6 +13,8 @@ function Editor:new(lvl)
 
     object.tilePanel = TilePanel:new(50, 50)
 
+    object.drawEnabled = false
+
     setmetatable(object, self)
     self.__index = self
     return object
@@ -23,6 +25,21 @@ function Editor:saveBaseImgData(filePath)
 end
 
 function Editor:update(dt)
+    if self.drawEnabled then
+        local x = love.mouse.getX()
+        local y = love.mouse.getY()
+        local imgx = math.floor((cam.x+x)/(self.world.scale*8))
+        local imgy = math.floor((cam.y+y)/(self.world.scale*8))
+        local color = selectedTile.imageDataColor
+        if selectedTile.name == "player" then
+            local r, g, b, a = self.world.baseImg:getPixel(imgx, imgy)
+            if (r == 0 and g == 0 and b == 0) then
+                self.world.baseImg:setPixel(imgx, imgy, color.r, color.g, color.b, 0)
+            end
+        else
+            self.world.baseImg:setPixel(imgx, imgy, color.r, color.g, color.b, 0)
+        end
+    end
     self.tilePanel:update(dt)
 end
 
@@ -43,21 +60,12 @@ end
 function Editor:mousepressed(x, y, button)
     if not self.tilePanel:mousepressed(x, y, button) then
         if selectedTile then
-            local imgx = math.floor((cam.x+x)/(self.world.scale*8))
-            local imgy = math.floor((cam.y+y)/(self.world.scale*8))
-            local color = selectedTile.imageDataColor
-            if selectedTile.name == "player" then
-                local r, g, b, a = self.world.baseImg:getPixel(imgx, imgy)
-                if (r == 0 and g == 0 and b == 0) then
-                    self.world.baseImg:setPixel(imgx, imgy, color.r, color.g, color.b, 0)
-                end
-            else
-                self.world.baseImg:setPixel(imgx, imgy, color.r, color.g, color.b, 0)
-            end
+            self.drawEnabled = true
         end
     end
 end
 
 function Editor:mousereleased(x, y, button)
     self.tilePanel:mousereleased(x, y, button)
+    self.drawEnabled = false
 end
